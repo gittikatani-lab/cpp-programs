6a5f2dfb72aa39f815e9bca721d7370c5a5f7322
Gitika <gittikatani@gmail.com>
2026-09-21 17:20:35 +0530

    Update ap.cpp

--- ap.cpp
+++ ap.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main() {
     int n;
     cout<<"enter n";
-    //1 5 9 13.....
+
     cin>>n;
     for(int i=1;i<=4*n-3;i=i+4){
         cout<<i<<endl;
-    }
+    }     //1 5 9 13.....
 }
