8fc645744bc00b135ca336f06fa2e03a51813722
Gitika <gittikatani@gmail.com>
2026-09-22 19:10:32 +0530

    Create comp_alpha.cpp

---
+++ comp_alpha.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"enter n ";
+    cin>>n;
+
+    for(int i= 1;i<=n;i++) {
+
+            cout<<(char)(i+64);
+            
+        }
+        cout<<endl;
+    }
