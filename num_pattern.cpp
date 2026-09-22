778e8e779fa21ab7b3572c48d4d6c2b6940ea45e
Gitika <gittikatani@gmail.com>
2026-09-22 19:05:48 +0530

    Create num_pattern.cpp

---
+++ num_pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"enter n ";
+    cin>>n;
+int m;
+    cout<<"enter m";
+    cin>>m;
+    for(int i= 1;i<=n;i++) {
+        for(int i=1;i<=m;i++) {
+            cout<<i;
+            
+        }
+        cout<<endl;
+    }
+}
